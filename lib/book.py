b889aa72e48e76e09b8655ce9470945511bdcab9
Densel <densel.esekon@moringaschool.com>
2024-01-21 21:48:15 +0300

    Completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
     
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+     # @property decorator is a built-in decorator in Python which is helpful in defining the properties effortlessly without manually calling the inbuilt function property(). Refer to this YT video for further explanation https://youtu.be/Sz9G0iwwHQI?si=gEaLS5iCkCLiJ9UJ
+    @property
+    def page_count(self):
+        # In Python, by convention, when you add a leading underscore to a name (self._page_count in this case), you are telling other developers that it should not be accessed or modified directly outside of the class. It should only be accessed through intermediaries (getters and setters) if they are available.
+        return self._page_count
+    
+    # Setter Method
+    @page_count.setter
+    def page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        self.size = size
+        self.brand = brand
+    # @property decorator is a built-in decorator in Python which is helpful in defining the properties effortlessly without manually calling the inbuilt function property(). Refer to this YT video for further explanation https://youtu.be/Sz9G0iwwHQI?si=gEaLS5iCkCLiJ9UJ
+      
+    # Getter Method
+    @property
+    def size(self):
+        # In Python, by convention, when you add a leading underscore to a name (self._size in this case), you are telling other developers that it should not be accessed or modified directly outside of the class. It should only be accessed through intermediaries (getters and setters) if they are available.
+        return self._size
+    
+    # Setter Method
+    @size.setter
+    def size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
